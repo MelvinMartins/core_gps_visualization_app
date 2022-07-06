cc65dd629868f504da160ca9d0f14709fa1e2464
MelvinMartins <melvin.martins@telecomnancy.net>
2022-07-06 15:40:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_gps_visualization_app",
-    version="1.0.7",
+    version="1.0.8",
     description="GPS Visualization for the core project",
     long_description=long_desc,
     author="NIST IT Lab",
